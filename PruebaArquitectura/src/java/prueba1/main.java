7354f4973a1abd547d3d8e70c0cee61aba56ef8f
MarcoQuimis <maquimis007@gmail.com>
2015-08-27 19:08:27 +0200

    Update main.java

--- PruebaArquitectura/src/java/prueba1/main.java
+++ PruebaArquitectura/src/java/prueba1/main.java
@@ -12,6 +12,6 @@ package prueba1;
 public class main {
     /*Esto lo edita el Usuario 1*/
     /* Esto lo edita el usuario 2*/
-    /*Esto lo hice despues*/
+    /*Esto lo hice despues desde arriba*/
     
 }
